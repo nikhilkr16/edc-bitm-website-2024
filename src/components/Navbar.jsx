751b5763ff7aed135a158cf005ec584b473b83e9
Anuj Nayak <anujnayak108@gmail.com>
2024-08-11 03:10:32 +0530

    added app page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const NavBar = () => {
                 <Link spy={true} smooth={true} to="/Team" onClick={handleClick}>
                     <li className="my-4 py-4 border-b border-transparent hover:bg-transparent hover:rounded transition">Team</li>
                 </Link>
-                <Link spy={true} smooth={true} to="App" onClick={handleClick}>
+                <Link spy={true} smooth={true} to="/App" onClick={handleClick}>
                     <li className="my-4 py-4 border-b border-transparent hover:bg-transparent hover:rounded transition">App</li>
                 </Link>
             </ul>
@@ -97,7 +97,7 @@ const NavBar = () => {
                                 <Link spy={true} smooth={true} to="/Team">
                                     <li className="hover:text-yellow-400 transition cursor-pointer">Team</li>
                                 </Link>
-                                <Link spy={true} smooth={true} to="App">
+                                <Link spy={true} smooth={true} to="/App">
                                     <li className="hover:text-yellow-400 transition cursor-pointer">App</li>
                                 </Link>
                             </ul>
--- src/otherpages/Apppage/Apppage.jsx
+++ src/otherpages/Apppage/Apppage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import img from '../Apppage/header1.png'
 
 function Apppage() {
     return(
-        <div></div>
+        <div className="bg-gray-300 flex flex-col justify-center items-center p-40 text-3xl">
+            <div className="bg-slate-200 flex flex-col justify-center items-center p-40 font-bold">
+            <img src={img} alt="" />
+            <h1>Coming Soon .....</h1>
+            </div>
+        </div>
     )
 }
+
+export default Apppage
