1ea98f8a25577c1b3a2cdcc2f7c1a29e550c3b59
Anuj Nayak <anujnayak108@gmail.com>
2024-08-12 11:40:41 +0530

    made event page

--- src/otherpages/Eventspage/Eventspage.jsx
+++ src/otherpages/Eventspage/Eventspage.jsx
@@ -28,52 +28,53 @@ import img1 from '../Eventspage/img1.png'
 
 function Eventspage() {
   return (
+    <>
       <div className="h-auto w-full text-black">
+        <div>
         <img src= {eventsbg} alt="" />
         {/* <div className="h-screen md:text-[50px] backdrop-brightness-50 flex flex-col justify-center items-center text-center"> */}
           {/* <h1 className="lg:px-48">Experience Exclusivity : Join Premium Club Events</h1> */}
         {/* </div> */}
+        </div>
+        <div className="flex justify-center recbox1">
+        <div className="bg-slate-500 w-[80%] p-8">
+        <div className="flex flex-row justify-start items-center">
+                <div className="h-[50px] w-[50px] bg-[#FED853]"></div>
+                <h1 className="text-[25px] ml-[-25px] font-bold">About Innovate-A-Thon</h1>
+            </div>
+            <div className="mt-12">
+                <p>Innovate-a-Thon is an annual event hosted by the Entrepreneurship Development Cell at BIT Mesra, offering budding innovators a chance to explore their technical talents and create something innovative during a 24-hour product development hackathon. With additional speaker sessions and workshops, the event aims to cultivate ideas and discussions, ultimately making a significant impact and driving transformative change.</p>
+            </div>
+            <div className="flex justify-center mt-4">
+            <button className="p-4 rounded-xl bg-[#FED853] font-bold">Register Now!</button>
+            </div>
+        </div>
+        </div>
       
-      <div className="bg-white w-screen overflow-hidden">zz
-        <div className="mt-[150px] text-center text-slate-700">
-          <h1 className="lg:text-[50px]">Unleash Wide Range of Exciting Events.</h1>
-        </div>
-        <div className="flex md:flex-row flex-col gap-8 justify-center items-center mt-20 md:p-20 p-8">
-        <div className="flex flex-col justify-center items-center text-center border-2 border-black p-4 pt-8 gap-6 rounded-2xl card1 text-white">
-            <h1 className="font-bold md:text-[37px]">Formal events</h1>
-            <p>Elevating Experiences with Distinguished Events</p>
-            <button className="bg-white p-2 rounded-3xl text-black">Explore More</button>
-            <img src={img1} alt="" srcset="" />
-        </div>
-        <div className="flex flex-col justify-center items-center text-center border-2 border-black p-4 pt-8 gap-6 rounded-2xl card3">
-            <h1 className="font-bold md:text-[37px]">Formal events</h1>
-            <p>Elevating Experiences with Distinguished Events</p>
-            <button className="bg-white p-2 rounded-3xl">Explore More</button>
-            <img src={img1} alt="" srcset="" />
-        </div>
-        <div className="flex flex-col justify-center items-center text-center border-2 border-black p-4 pt-8 gap-6 rounded-2xl card2 text-white ">
-            <h1 className="font-bold md:text-[37px]">Formal events</h1>
-            <p>Elevating Experiences with Distinguished Events</p>
-            <button className="bg-white p-2 rounded-3xl text-black">Explore More</button>
-            <img src={img1} alt="" srcset="" />
-        </div>
-        
-        </div>
+      <div className="bg-white w-screen overflow-hidden mt-32">
         <div className="flex lg:flex-row bg-white p-20 text-center justify-center items-center">
-          <img src={leaf1} alt="" />
+          <img src={leaf1} alt="" className="md:h-56"/>
           <p className="md:text-[32px]">Our Events are more than just gatherings. They are opportunities to transform your entrepreneurial journey. Unleash the Entrepreneur in you and connect with us for the latest updates and exclusive announcements.!</p>
-          <img src={leaf2} alt="" />
-        </div>
-        <div className="flex flex-row justify-center items-center gap-6">
-          <a href="https://facebook.com/edcbitmesra/"><FaFacebookSquare className="w-14 h-14" /></a>
-          <a href="https://www.instagram.com/edcbitmesra/"><FaInstagram className="w-14 h-14" /></a>
-          <a href="https://www.linkedin.com/company/edcbitmesra/"><FaLinkedin className="w-14 h-14" /></a>
+          <img src={leaf2} alt="" className="md:h-56"/>
         </div>
       </div>
       <div className="md:m-20 m-4 mt-20">
         <div className="flex flex-col justify-center items-center lg:m-4 p-4 md:p-2 shadow-2xl rounded-2xl">
-          <h1 className="font-bold text-[32px] uppercase">formal events</h1>
+          <h1 className="font-bold text-[32px] uppercase">Past Events</h1>
           <div className="flex md:flex-row flex-col justify-center ">
+          <div className="event-card-container">
+              <div className="event-card">
+                <img src={event6} alt="Event" className="event-image" />
+                <div className="event-details">
+                  <div className="event-header">
+                    <h2>STOCK STOCISM</h2>
+                    <button className="participate-button">PARTICIPATE</button>
+                  </div>
+                  <p>VENUE: ROOM 312</p>
+                  <h4>DATE: 24/03/24</h4>
+                </div>
+              </div>
+            </div>
             <div className="event-card-container">
               <div className="event-card">
                 <img src={event1} alt="Event" className="event-image" />
@@ -89,23 +90,10 @@ function Eventspage() {
             </div>
             <div className="event-card-container">
               <div className="event-card">
-                <img src={event6} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>STOCK STOCISM</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            <div className="event-card-container">
-              <div className="event-card">
                 <img src={event3} alt="Event" className="event-image" />
                 <div className="event-details">
                   <div className="event-header">
-                    <h2>FANTASY START</h2>
+                    <h2>INNOVATE-A-THON</h2>
                     <button className="participate-button">PARTICIPATE</button>
                   </div>
                   <p>VENUE: ROOM 312</p>
@@ -115,6 +103,19 @@ function Eventspage() {
             </div>
           </div>
           <div className="flex md:flex-row flex-col justify-center">
+          <div className="event-card-container">
+              <div className="event-card">
+                <img src={event14} alt="Event" className="event-image" />
+                <div className="event-details">
+                  <div className="event-header">
+                    <h2>IPL AUCTION</h2>
+                    <button className="participate-button">PARTICIPATE</button>
+                  </div>
+                  <p>VENUE: ROOM 312</p>
+                  <h4>DATE: 24/03/24</h4>
+                </div>
+              </div>
+            </div>
           <div className="event-card-container">
               <div className="event-card">
                 <img src={event4} alt="Event" className="event-image" />
@@ -128,147 +129,14 @@ function Eventspage() {
                 </div>
               </div>
             </div>
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event2} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>INVESTOR'S HAT</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
-      <div className="md:m-20 m-4 mt-20">
-      <div className="flex flex-col justify-center items-center lg:m-4 p-4q lg:p-2 shadow-2xl rounded-2xl text-center">
-      <h1 className="font-bold text-[32px] uppercase">semi-formal events</h1>
-          <div className="flex md:flex-row flex-col justify-center">
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event7} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>BIT-THON</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event8} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>ANCIENT ENTP</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event9} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>MARKETING SPINOFF</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex md:flex-row flex-col justify-center">
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event10} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>QUIZ TRIVIA</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            </div>
-        </div>
-      </div>
-      <div className="md:m-12 m-4 mt-20">
-      <div className="flex flex-col justify-center items-center lg:m-4 p-4 lg:p-2  shadow-2xl rounded-2xl">
-      <h1 className="font-bold text-[32px] uppercase">informal events</h1>
-          <div className="flex md:flex-row flex-col justify-center ">
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event12} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>BGMI</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event13} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>VALORANT</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-            <div className="event-card-container">
-              <div className="event-card">
-                <img src={event14} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>IPL AUCTION</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex md:flex-row flex-col justify-center ">
-          <div className="event-card-container">
-              <div className="event-card">
-                <img src={event5} alt="Event" className="event-image" />
-                <div className="event-details">
-                  <div className="event-header">
-                    <h2>FIFA CONTEST</h2>
-                    <button className="participate-button">PARTICIPATE</button>
-                  </div>
-                  <p>VENUE: ROOM 312</p>
-                  <h4>DATE: 24/03/24</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      
       <Slidermain />
       <Socials />
     </div>
+    </>
   )
 }
 
