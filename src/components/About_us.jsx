57f809275074ddc32cddf16ded5fea49bba3220f
Abhinav Kumar Choudhary <127733274+Abhi1264@users.noreply.github.com>
2024-08-14 04:03:17 +0530

    Update About_us.jsx

--- src/components/About_us.jsx
+++ src/components/About_us.jsx
@@ -9,7 +9,7 @@ function About_us() {
           <div className="glass-morphism md:w-[80%] w-full md:p-8 p-8">
             <div className="flex flex-row justify-start items-center">
               <div className="h-[50px] w-[50px] bg-[#FED853]"></div>
-              <h1 className="text-[25px] ml-[-35px] font-bold">About Innovate-A-Thon</h1>
+              <h1 className="text-[25px] ml-[-35px] font-bold">About Us</h1>
             </div>
             <p className='mt-5 text-lg'>The Entrepreneurship Cell (E-Cell) at BIT MESRA
               is a student-driven initiative dedicated to promoting entrepreneurship among students. Through workshops, events, and mentorship programs, it cultivates a culture of innovation
