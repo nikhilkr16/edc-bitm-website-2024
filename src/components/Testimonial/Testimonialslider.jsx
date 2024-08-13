d74a508c5c316142abc7d8d01f132f40fe1d03d2
Nikhil Kumar <66540592+nikhilkr16@users.noreply.github.com>
2024-08-14 02:42:14 +0530

    Update Testimonialslider.jsx

--- src/components/Testimonial/Testimonialslider.jsx
+++ src/components/Testimonial/Testimonialslider.jsx
@@ -18,8 +18,8 @@ function Testimonialslider() {
 		slidesToScroll: 1,
 		initialSlide: 2,
 		autoplay: true,
-		speed: 2000,
-		autoplaySpeed: 5000,
+		speed: 1000,
+		autoplaySpeed: 1000,
 		cssEase: "linear",
 	};
 	return (
