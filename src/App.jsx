1da5fb2c7f476b8962b36b764aeece2e60894ba7
Abhi1264 <abhinav.kumar.1264@gmail.com>
2024-08-14 16:27:39 +0530

    minor bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <><NavBar/><Home/></>
+      element: <><NavBar/><Home/><Footer/></>
     },
     {
       path: "/Speakers",
