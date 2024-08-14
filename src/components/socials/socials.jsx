50b55715431975ae6e55b600234137481ebcb22b
Abhi1264 <abhinav.kumar.1264@gmail.com>
2024-08-14 16:38:26 +0530

    added text shadow in social handles section

--- src/components/socials/socials.jsx
+++ src/components/socials/socials.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { FaInstagram, FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
-import '../socials/socials.css'
+import '../socials/socials.css';
 
 function Socials() {
     return (
-            <div className="socialbg w-screen border-0 bg-cover text-white">
-                <div className="backdrop-brightness-[0.25]">
+        <div className="socialbg w-screen bg-cover text-white">
+            <div className="backdrop-brightness-[0.25]">
                 <div className="flex flex-row justify-center items-center p-12">
                     <div className="h-[50px] w-[50px] bg-[#FED853]"></div>
-                    <h1 className="text-[25px] ml-[-30px] font-bold ">Social Handles</h1>
+                    <h1 className="text-[25px] ml-[-30px] font-bold" style={{ textShadow: '2px 2px 5px rgba(0, 0, 0, 0.5)' }}>
+                        Social Handles
+                    </h1>
                 </div>
-                <div className="text-center text-[45px] font-bold">Subscribe To Get The Latest News And Updates</div>
+                <div className="text-center text-[45px] font-bold" style={{ textShadow: '2px 2px 5px rgba(0, 0, 0, 0.5)' }}>
+                    Subscribe To Get The Latest News And Updates
+                </div>
                 <div className="flex flex-col sm:flex-row justify-center items-center space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 p-12">
                     <a href="https://www.instagram.com/edcbitmesra" target="_blank" rel="noopener noreferrer">
                         <button className="bg-white text-black border-4 border-[#FED853] px-10 py-4 flex items-center justify-center space-x-2 rounded-xl text-lg">
@@ -31,8 +35,8 @@ function Socials() {
                         </button>
                     </a>
                 </div>
-                </div>
             </div>
+        </div>
     );
 }
 
